e6cfc15d32f65eef866b9d168c996fb9893bc116
liwenyang <1286203036@qq.com>
2022-07-18 18:02:11 +0800

    新增了包含response的基础方法的base.go

---
+++ cmd/api/controller/base.go
@@ -0,0 +1,28 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/Ligouhai-bigone/easy_douyin/pkg/constants/errno"
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Code    int64       `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func SendResponse(c *gin.Context, err error, data interface{}) {
+	Err := errno.ConvertErr(err)
+	c.JSON(http.StatusOK, Response{
+		Code:    Err.ErrCode,
+		Message: Err.ErrMsg,
+		Data:    data,
+	})
+}
+
+type UserParam struct {
+	UserName string `json:"username"`
+	PassWord string `json:"password"`
+}
