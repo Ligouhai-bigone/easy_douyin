04786dfa9dd656a3d82469b7c07564781b5866e8
liwenyang <1286203036@qq.com>
2022-07-22 18:25:10 +0800

    增加redis key判断

--- cmd/user/dal/cache/user.go
+++ cmd/user/dal/cache/user.go
@@ -3,9 +3,14 @@ package cache
 import (
 	"context"
 	"fmt"
+
+	"github.com/go-redis/redis/v8"
 )
 
 func RedisSet(ctx context.Context, key string, value string) error {
+	if _, err := Redisclient.Get(ctx, key).Result(); err != redis.Nil {
+		fmt.Println("Redis client set successfully...")
+	}
 	err := Redisclient.Set(ctx, key, value, 0).Err()
 	if err != nil {
 		panic(err)
@@ -17,6 +22,11 @@ func RedisSet(ctx context.Context, key string, value string) error {
 
 func RedisGetKey(ctx context.Context, key string) (string, error) {
 	value, err := Redisclient.Get(ctx, key).Result()
+
+	if err == redis.Nil {
+		return value, err
+	}
+
 	if err != nil {
 		panic(err)
 	}
