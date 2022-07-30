eae1ced892fc5080fdf7b2cda72f6fdba34b999b
liwenyang <1286203036@qq.com>
2022-07-30 13:00:47 +0800

    设置key的过期时间3分钟;增加判断是否在set的功能;增加获取set的功能

--- cmd/user/dal/cache/user.go
+++ cmd/user/dal/cache/user.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -11,7 +12,7 @@ func RedisSetKey(ctx context.Context, key string, value interface{}) error {
 	if _, err := Redisclient.Get(ctx, key).Result(); err != redis.Nil {
 		fmt.Println("Redis client set successfully...")
 	}
-	err := Redisclient.Set(ctx, key, value, 0).Err()
+	err := Redisclient.Set(ctx, key, value, 3*time.Minute).Err()
 	if err != nil {
 		panic(err)
 	}
@@ -35,7 +36,7 @@ func RedisGetKey(ctx context.Context, key string) (string, error) {
 	return value, nil
 }
 
-func RedisSetZ(ctx context.Context, key string, value interface{}) error {
+func RedisSetS(ctx context.Context, key string, value interface{}) error {
 	err := Redisclient.SAdd(ctx, key, value).Err()
 	if err != nil {
 		panic(err)
@@ -44,11 +45,29 @@ func RedisSetZ(ctx context.Context, key string, value interface{}) error {
 	return nil
 }
 
-func RedisDeleteZ(ctx context.Context, key string, value interface{}) error {
+func RedisDeleteS(ctx context.Context, key string, value interface{}) error {
 	err := Redisclient.SRem(ctx, key, value).Err()
 	if err != nil {
 		panic(err)
 	}
 
 	return nil
+}
+
+func RedisGetS(ctx context.Context, key string) ([]string, error) {
+	value, err := Redisclient.SMembers(ctx, key).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	return value, nil
+}
+
+func RedisIsInS(ctx context.Context, key string, value interface{}) (bool, error) {
+	isIn, err := Redisclient.SIsMember(ctx, key, value).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	return isIn, nil
 }
